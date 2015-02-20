44650aae1576de8141d8b5ef6abce375a94ea573
John Bernier <john.b.bernier@gmail.com>
2015-02-19 22:40:03 -0500

    added some code to help debug note placement issue

--- src/score_panel.js
+++ src/score_panel.js
@@ -28,7 +28,7 @@ live_score.Score_panel.prototype.create_score_canvas = function(){
   this.score_canvas = document.createElement('canvas');
   this.score_canvas.id = 'score_panel';
   this.score_canvas.width = 1000;
-  this.score_canvas.height = 500;
+  this.score_canvas.height = 200;
 };
 
 live_score.Score_panel.prototype.attach_score_panel = function(parent_element){
